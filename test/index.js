f551321d48bb516dcc213259325b9f8cec59af90
Dali Zheng <dali@series.ac>
2015-07-24 01:59:50 +0200

    improve test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import { comment, pass, fail, end, run } from '../lib'
 import assert from 'assert'
 
-run(() => {
+run.only(() => {
   comment('assert')
   try {
     assert(true)
@@ -14,10 +14,12 @@ run(() => {
   }
 })
 
-run(() => {
+run.only(() => {
   comment('function check')
   pass(() => assert(true), 'works')
   fail(() => assert(false), 'should throw')
 })
 
-run(end)
+run(() => fail('run.only doesn\'t work'))
+
+run.only(end)
