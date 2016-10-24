745b0636c2f3379fb9d2013f99843305e741a113
0x8890 <0x8890@airmail.cc>
2016-10-24 17:56:10 +0200

    shorten extract program, maybe it should be a separate module

--- extract.js
+++ extract.js
@@ -1,17 +1,8 @@
 'use strict'
 
-const readline = require('readline')
+var indent = /^    /
 
-const rl = readline.createInterface({ input: process.stdin })
-let isBlock = false
-
-rl.on('line', input => {
-  if (!input.length) {
-    if (!isBlock) isBlock = true
-    else isBlock = false
-    return
-  }
-
-  if (/^    /.test(input) && isBlock) console.log(input.trim())
-  else isBlock = false
-})
+require('readline')
+.createInterface({ input: process.stdin })
+.on('line', input => indent.test(input) ?
+  process.stdout.write(`${input.replace(indent, '')}\n`) : null)
