23d804be712b7b5fa5e74bd4ee6cb6a20cc7fe58
0x8890 <0x8890@airmail.cc>
2016-10-25 22:23:26 +0200

    packaging tweaks

--- extract.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var indent = /^    /
-
-require('readline')
-.createInterface({ input: process.stdin })
-.on('line', input => indent.test(input) ?
-  process.stdout.write(`${input.replace(indent, '')}\n`) : null)
